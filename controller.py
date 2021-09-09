d22be0a877045ca16c8c7f887cc614a585c20cbd
Vusal <vusal581176@gmail.com>
2021-09-10 00:30:59 +0400

    lastCOmmit

--- controller.py
+++ controller.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, request, url_for
 from app import app
-from models import Blog, Comment
-from forms import Blog_info, Blog_comments
+from models import Blog
+from forms import Blog_info
 
 
 
@@ -23,22 +23,3 @@ def blog():
             return redirect(url_for("blogs"))
 
     return render_template('blog.html', form=form)
-
-
-@app.route("/blogs/<int:book_id>")
-def blog_ids(book_id):
-    blogs = Blog.query.get(book_id)
-    return render_template('blog2.html', blogs=blogs)
-
-@app.route("/blogs/comments", methods=['GET', 'POST'])
-def blog_comment():
-    post_data=request.form
-    form = Blog_comments()
-    form = Blog_comments(data=post_data)
-    if request.method=='POST':
-        if form.validate_on_submit():
-            comments = Comment(content=form.content.data, blog_id=form.blog_id.data)
-            comments.save()
-            return redirect(url_for("blog_comment"))
-
-    return render_template('blog3.html', form=form)
--- forms.py
+++ forms.py
@@ -6,7 +6,3 @@ from wtforms.validators import DataRequired, Length
 class Blog_info(FlaskForm):
     title = StringField('Blog title:', validators=[DataRequired(), Length(max=30)] )
     description = TextAreaField('Description:', validators=[Length(max=50)])
-
-class Blog_comments(FlaskForm):
-    content = StringField('Comment content:', validators=[DataRequired(), Length(max=30)] )
-    blog_id = IntegerField('Blog id:', validators=[DataRequired()])
